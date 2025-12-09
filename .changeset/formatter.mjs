d8a6c047358bda2977f1ae8a3a4b3a931d4c2bcd
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-09 16:29:08 +0100

    Renamed function

--- .changeset/formatter.mjs
+++ .changeset/formatter.mjs
@@ -1,3 +1,3 @@
-import getConfig from '@ijlee2-frontend-configs/changesets';
+import getFormatter from '@ijlee2-frontend-configs/changesets';
 
-export default getConfig('ijlee2/analyze-ember-project-dependencies');
+export default getFormatter('ijlee2/analyze-ember-project-dependencies');
